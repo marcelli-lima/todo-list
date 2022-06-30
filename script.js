3ce5fae57aa1b5a44ca5b3005d7d6c19f04b8c7b
marcelli-lima <marcelli_lima@hotmail.com>
2022-06-30 12:31:24 -0300

    correçoes a partir do lint

--- script.js
+++ script.js
@@ -22,19 +22,19 @@ button.addEventListener('click', addTarefa);
 const list = document.getElementById('lista-tarefas');
 list.addEventListener('click', (event) => {
   const lis = document.getElementsByTagName('li');
-  for (li of lis) {
-    if (li.style.backgroundColor = 'rgb(128, 128, 128)') {
-      li.style.backgroundColor = 'white';
+  for (const li of lis) {
+    if (li.className === 'selected') {
+      li.classList.remove('selected');
     }
   }
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  event.target.classList.add('selected');
 });
 
 list.addEventListener('dblclick', (event) => {
-  if (event.target.className === 'completed') {
+  if (event.target.classList.contains('completed')) {
     event.target.classList.remove('completed');
   } else {
-    event.target.className = 'completed';
+    event.target.classList.add('completed');
   }
 });
 
@@ -50,3 +50,40 @@ buttonRemoveCompleted.addEventListener('click', () => {
     taskCompleted[i].remove();
   }
 });
+
+const btnMoveUp = document.querySelector('#mover-cima');
+btnMoveUp.addEventListener('click', () => {
+  const selected = document.querySelector('.selected');
+  if (selected && list.length !== 0 && selected.previousElementSibling !== null) {
+    document.querySelector('ol').insertBefore(selected, selected.previousElementSibling);
+  }
+});
+
+const btnMoveDown = document.querySelector('#mover-baixo');
+btnMoveDown.addEventListener('click', () => {
+  const selected = document.querySelector('.selected');
+  if (selected && list.length !== 0 && selected.nextElementSibling !== null) {
+    document.querySelector('ol').insertBefore(selected.nextElementSibling, selected);
+  }
+});
+
+const buttonRemoveSelected = document.getElementById('remover-selecionado');
+buttonRemoveSelected.addEventListener('click', () => {
+  const lis = document.getElementsByTagName('li');
+  for (const selected of lis) {
+    if (selected.className === 'selected') {
+      selected.remove();
+    }
+  }
+});
+const buttonSaveTask = document.getElementById('salvar-tarefas');
+buttonSaveTask.addEventListener('click', () => {
+  localStorage.clear();
+  const todasTarefas = list.innerHTML;
+  localStorage.setItem('listaSalva', todasTarefas);
+});
+
+function recuperarListaSalva() {
+  list.innerHTML = localStorage.getItem('listaSalva');
+}
+recuperarListaSalva();
